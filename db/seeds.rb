eb6c966cac254b733a66168934c0f582285c7c3e
b264 <public88878878887@hotmail.com>
2016-10-22 21:57:27 +0000

    add shelters

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,7 +9,6 @@
 Gender.where(description: 'women').first_or_create!
 Gender.where(description: 'men').first_or_create!
 Gender.where(description: 'family').first_or_create!
-
 Shelter.where(name: %q[ALIVE (Alternatives to Living in a Violent Environment)]).first_or_create!.update!(
   href: %q[http://www.alivestl.org/],
   description: %q[Temporary emergency sanctuary and transportation to victims of domestic violence, 24 hours a day, 365 days a year. Night of Safety plus counseling, interventions, legal advocacy.],
@@ -43,7 +42,6 @@ Shelter.where(name: %q[ALIVE (Alternatives to Living in a Violent Environment)])
     )}
   ],
 )
-
 Shelter.where(name: %q[BRIDGEWAY]).first_or_create!.update!(
   href: %q[http://www.bridgewaycounseling.com/],
   description: %q[Bridgeway operates the only two emergency safe shelters and outreach centers in St. Charles and Lincoln Counties for women and children who are victims of domestic violence.],
@@ -77,7 +75,6 @@ Shelter.where(name: %q[BRIDGEWAY]).first_or_create!.update!(
     )}
   ],
 )
-
 Shelter.where(name: %q[COVENANT HOUSE]).first_or_create!.update!(
   href: %q[http://www.covenanthouse.org/],
   description: %q[Various locations for teens in crisis. The Covenant House NINELINE (800-999-9999) is a free, confidential crisis hotline that operates 24 hours a day, 365 days a year. The Covenant House NINELINE is also available on the Internet at www.NINELINE.org where young people can submit questions via e-mail or participate in forums.],
@@ -95,6 +92,119 @@ Shelter.where(name: %q[COVENANT HOUSE]).first_or_create!.update!(
       ).first_or_create!,
     ).first_or_create!.tap{|l| l.update!(
       phone_number: PhoneNumber.where(number: '+1.314.533.2451').first_or_create!
+    )},
+  ],
+)
+Shelter.where(name: %q[GATEWAY 180: HOMELESSNESS REVERSED]).first_or_create!.update!(
+  href: %q[http://www.gateway180.org/],
+  description: %q[The shelter provides emergency shelter services designed to get families into transitional or permanent homes in under 30 days. The shelter provides comprehensive support services targeted to meeting its clients’ specific needs. The organization works with Affinia/Grace Hill and Washington University to provide health services.],
+  genders: [Gender.women],
+  locations: [
+    Location.where(
+      description: %q[],
+      address: Address.where(
+        line1: %q[1000 North 19th St.],
+        line2: %q[],
+        city: %q[],
+        state: %q[],
+        postal_code: %q[63106],
+        country: %q[USA],
+      ).first_or_create!,
+    ).first_or_create!.tap{|l| l.update!(
+      phone_number: PhoneNumber.where(number: '+1.314.231.1515').first_or_create!
+    )},
+  ],
+)
+Shelter.where(name: %q[GRACE AND PEACE WINTER SHELTER FOR WOMEN]).first_or_create!.update!(
+  href: %q[],
+  description: %q[Operating this year until March 12.  Provides shelter 6:30 pm-6:00 am November-March for 16 homeless women in the church's basement.  Mental health professional sees the women once a week, and the women are given help looking for jobs and housing.],
+  genders: [Gender.women],
+  locations: [
+    Location.where(
+      description: %q[],
+      address: Address.where(
+        line1: %q[5574 Delmar],
+        line2: %q[(near Union)],
+        city: %q[],
+        state: %q[],
+        postal_code: %q[],
+        country: %q[USA],
+      ).first_or_create!,
+    ).first_or_create!.tap{|l| l.update!(
+      phone_number: PhoneNumber.where(number: '+1.314.367.8959').first_or_create!
+    )},
+  ],
+)
+Shelter.where(name: %q[HAVEN OF GRACE]).first_or_create!.update!(
+  href: %q[http://www.havenofgracestl.org/],
+  description: %q[Residential facility that assists homeless, pregnant women with shelter, goal-setting, education, employment, parenting, household management, and permanent residence.],
+  genders: [Gender.women],
+  locations: [
+    Location.where(
+      description: %q[],
+      address: Address.where(
+        line1: %q[1225 Warren],
+        line2: %q[],
+        city: %q[],
+        state: %q[],
+        postal_code: %q[63106],
+        country: %q[USA],
+      ).first_or_create!,
+    ).first_or_create!.tap{|l| l.update!(
+      phone_number: PhoneNumber.where(number: '+1.314.621.6507').first_or_create!
+    )},
+  ],
+)
+Shelter.where(name: %q[KAREN HOUSE]).first_or_create!.update!(
+  href: %q[http://www.karenhousecw.org/],
+  description: %q[Karen House is a Catholic Worker house of hospitality for homeless women and children.  They only consider new clients for housing on Mondays after 9 AM. CALL FIRST to see if there are any openings.],
+  genders: [Gender.women],
+  locations: [
+    Location.where(
+      description: %q[],
+      address: Address.where(
+        line1: %q[1840 Hogan St.],
+        line2: %q[],
+        city: %q[],
+        state: %q[],
+        postal_code: %q[63106],
+        country: %q[USA],
+      ).first_or_create!,
+    ).first_or_create!.tap{|l| l.update!(
+      phone_number: PhoneNumber.where(number: '+1.314.621.4052').first_or_create!
+    )},
+  ],
+)
+Shelter.where(name: %q[KATHY WEINMAN CENTER]).first_or_create!.update!(
+  href: %q[http://www.keepingkidsfirst.org/],
+  description: %q[Temporary, emergency shelter, court advocacy, professional therapy for women and children, support groups. Follow-up supportive services available. St. Louis County operates this shelter.],
+  genders: [Gender.women],
+  locations: [
+    Location.where(
+      description: %q[],
+      address: nil,
+    ).first_or_create!.tap{|l| l.update!(
+      phone_number: PhoneNumber.where(number: '+1.314.423.1117').first_or_create!
+    )},
+  ],
+)
+Shelter.where(name: %q[LOAVES AND FISHES]).first_or_create!.update!(
+  href: %q[http://loavesandfishes-stl.org/food-pantry/],
+  description: %q[30-day emergency shelter for women and children. Many services offered. Housing Resource Center referral is required.],
+  genders: [Gender.women],
+  locations: [
+    Location.where(
+      description: %q[],
+      address: Address.where(
+        line1: %q[2750 McKelvey Rd.],
+        line2: %q[],
+        city: %q[Maryland Heights],
+        state: %q[MO],
+        postal_code: %q[63043],
+        country: %q[USA],
+      ).first_or_create!,
+    ).first_or_create!.tap{|l| l.update!(
+      phone_number: PhoneNumber.where(number: '+1.314.291.3857').first_or_create!
     )},
   ],
 )
