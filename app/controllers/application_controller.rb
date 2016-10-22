e357dd241f057855f0905ef8b8e70df1e6c479b7
b264 <public88878878887@hotmail.com>
2016-10-22 17:24:56 +0000

    json

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,16 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  #respond_to :json
+  FOO= {
+      requestId: nil,
+      likelihood: nil,
+      photos: nil,
+  }
+  def root
+    respond_to do |format|
+      format.json do
+        render json: FOO
+      end
+    end
+  end
 end
