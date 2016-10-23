71b2f8bc4556412469606f66b73369e34035dab5
b264 <public88878878887@hotmail.com>
2016-10-23 13:22:44 +0000

    use array

--- app/controllers/cases_controller.rb
+++ app/controllers/cases_controller.rb
@@ -19,16 +19,16 @@ class CasesController < ApiController
   ]
 
   def open
-    render json: {case_number: case_number, subject: SUBJECTS.sample, start_date: start_date}
+    render json: {open_cases_array: [{case_number: case_number, subject: SUBJECTS.sample, start_date: start_date},{case_number: case_number, subject: SUBJECTS.sample, start_date: start_date}]}
   end
 
 private
 
-  def case_number; def self.case_number; @case_number end
+  def case_number#; def self.case_number; @case_number end
     @case_number= CASE_NUMBER_PREFIXES.sample+ rand(10000..99999).to_s
   end
 
-  def start_date; def self.start_date; @start_date end
+  def start_date#; def self.start_date; @start_date end
     @start_date= (Time.current - rand(0..99)*24*60*60).strftime '%m/%d/%y'
   end
 end
