5209444e143fcacb659d44631a2c53ba72441786
b264 <public88878878887@hotmail.com>
2016-10-23 07:36:24 +0000

    case number

--- app/controllers/cases_controller.rb
+++ app/controllers/cases_controller.rb
@@ -1,6 +1,34 @@
 class CasesController < ApiController
+  CASE_NUMBER_PREFIXES= [
+    '#c',
+    '#B',
+    '#D',
+    '#d',
+    '#e',
+    '#E',
+    '#B',
+    '#c',
+    '#d',
+    '#d',
+    '#g',
+    '#G',
+    '#T',
+  ]
+  SUBJECTS= [
+    'Assisted Housing',
+  ]
 
   def open
-    render json: {case_number: '#c45645',subject: 'Assisted Housing',start_date: (Time.current - 20*24)}
+    render json: {case_number: case_number, subject: SUBJECTS.sample, start_date: start_date}
+  end
+
+private
+
+  def case_number; def self.case_number; @case_number end
+    @case_number= CASE_NUMBER_PREFIXES.sample+ rand(10000..99999).to_s
+  end
+
+  def start_date; def self.start_date; @start_date end
+    @start_date= (Time.current - rand(0..99)*24*60*60).strftime '%m/%d/%y'
   end
 end
